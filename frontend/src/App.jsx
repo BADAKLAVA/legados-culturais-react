90d0a4061d3a0aa6db4ed887dc0a720a95b7c794
DanTheBrazuca <dars120308@gmail.com>
2026-09-18 09:40:24 -0300

    Replace buttons with select dropdowns for options

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -200,9 +200,34 @@ function Cadastro() {
             <label className="field state-field">
               <span>Estado</span>
               <select className="select-button">
-                Selecionar
-                <option value="someOption">Some option</option>
-                <option value="otherOption">Other option</option>
+                <option value="">Selecionar</option>
+                <option value="AC">AC</option>
+                <option value="AL">AL</option>
+                <option value="AP">AP</option>
+                <option value="AM">AM</option>
+                <option value="BA">BA</option>
+                <option value="CE">CE</option>
+                <option value="DF">DF</option>
+                <option value="ES">ES</option>
+                <option value="GO">GO</option>
+                <option value="MA">MA</option>
+                <option value="MT">MT</option>
+                <option value="MS">MS</option>
+                <option value="MG">MG</option>
+                <option value="PA">PA</option>
+                <option value="PB">PB</option>
+                <option value="PR">PR</option>
+                <option value="PE">PE</option>
+                <option value="PI">PI</option>
+                <option value="RJ">RJ</option>
+                <option value="RN">RN</option>
+                <option value="RO">RO</option>
+                <option value="RR">RR</option>
+                <option value="RS">RS</option>
+                <option value="SC">SC</option>
+                <option value="SP">SP</option>
+                <option value="SE">SE</option>
+                <option value="TO">TO</option>
               </select>
             </label>
             <Input label="Cidade" placeholder="Cidade" className="city-field" />
@@ -233,9 +258,10 @@ function Login() {
           <h1>Entrar</h1>
           <div className="account-type">
             <span>Tipo de conta:</span>
-            <Button type="button" className="select-button">
-              Usuário
-            </Button>
+            <select className="select-button">
+                <option value="User">Usuário</option>
+                <option value="Adm">Admin</option>
+            </select>
           </div>
           <Input label="E-mail" placeholder="E-mail" type="email" />
           <Input label="Senha" placeholder="Senha" type="password" />
